554c994336d1783673cd85f59aa13b90ee48137d
Rok Mocnik <rok.mocnik@sinergise.com>
2017-05-19 10:09:28 +0200

    up version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ EXTRA_REQUIRES = parse_requirements("requirements-dev.txt")
 
 setup(
     name='sentinelhub',
-    version='0.1',
+    version='0.1.1',
     description='Sentinel Hub Utilities',
     url='https://github.com/sinergise/sentinelhub',
     author='Sinergise ltd.',
