fde0d46a66dc7f259c2182cbb5adbaf256db2c36
Matej Aleksandrov <matej.aleksandrov@sinergise.com>
2017-05-22 15:12:13 +0200

    tests

--- sentinelhub/tests/test_download.py
+++ sentinelhub/tests/test_download.py
@@ -2,11 +2,13 @@ import pytest
 
 from sentinelhub import download
 
-'''def test_download():
-    assert download("UTM31") == "Downloading UTM31"
+def test_download():
+    pass
+    #assert download("UTM31") == "Downloading UTM31"
 
 def test_no_input():
-    with pytest.raises(TypeError):
-        download()'''
+    pass
+    #with pytest.raises(TypeError):
+    #    download()
 
 # S2A_OPER_PRD_MSIL1C_PDMC_20151219T140443_R135_V20151219T080616_20151219T080616
