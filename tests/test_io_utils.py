d4524e409ad2c6d7225ab194a2a53c16851a9389
AleksMat <matej.aleksandrov@sinergise.com>
2018-07-09 15:20:22 +0200

    io_utils unittests updated

--- tests/test_io_utils.py
+++ tests/test_io_utils.py
@@ -1,8 +1,9 @@
 import unittest
+import os.path
 import numpy as np
 from tests_all import TestSentinelHub
 
-from sentinelhub.io_utils import read_data
+from sentinelhub.io_utils import read_data, write_data
 
 
 class TestIO(TestSentinelHub):
@@ -12,12 +13,18 @@ class TestIO(TestSentinelHub):
         formats = [('tif', 13577.49), ('jpg', 52.41), ('png', 52.34), ('jp2', 47.09)]
         for ext, exp_mean in formats:
             with self.subTest(msg=ext):
-                filepath = '{}/ml.{}'.format(self.INPUT_FOLDER, ext)
-                img = read_data(filepath)
+                filename = os.path.join(self.INPUT_FOLDER, 'ml.{}'.format(ext))
+                img = read_data(filename)
                 self.assertEqual(np.shape(img), (W, H, 3) if ext != 'jp2' else (343, 343, 3),
                                  "{} image dimension mismatch".format(ext))
                 self.assertAlmostEqual(np.mean(img), exp_mean, delta=1e-1,
                                        msg="{} image has incorrect values".format(ext))
+
+                new_filename = os.path.join(self.OUTPUT_FOLDER, os.path.split(filename)[-1])
+                write_data(new_filename, img)
+                new_img = read_data(new_filename)
+                if ext != 'jpg':
+                    self.assertTrue(np.array_equal(img, new_img), msg="Original and new image are not the same")
 
 
 if __name__ == '__main__':
