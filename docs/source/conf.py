cd64385375ee4a989fb6b0ddf7c22eb8a6877a7f
Tomi Slijepcevic <tomi.slijepcevic@gmail.com>
2018-08-09 13:51:24 +0200

    In the documentation add links to source code.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,15 +29,17 @@ import shutil
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'nbsphinx',
     'sphinx.ext.mathjax',
-    #'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
