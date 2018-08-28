e624a883e51ee381a18e471030b165d8e38fdfe7
AleksMat <matej.aleksandrov@sinergise.com>
2018-08-28 08:27:43 +0200

    docs config fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,9 +54,7 @@ extensions = [
     'nbsphinx',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
-    'IPython.sphinxext.ipython_console_highlighting',
-    'm2r'
+    'sphinx.ext.githubpages'
 ]
 
 # Both the class’ and the __init__ method’s docstring are concatenated and inserted.
