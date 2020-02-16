8496706ea202a703f4affc7962525b8c826c68e2
iovsn <jovan.visnjic@sinergise.com>
2020-02-16 16:05:12 +0100

    support Processing API's resolution parameter

--- tests/test_sentinelhub_request.py
+++ tests/test_sentinelhub_request.py
@@ -53,6 +53,49 @@ class TestSentinelHubRequest(TestSentinelHub):
         self.assertEqual(img.shape, (856, 512, 3))
         self.test_numpy_data(img, exp_min=0, exp_max=255, exp_mean=74.92)
 
+    def test_resolution_parameter(self):
+        """ Test downloading three bands of L1C
+        """
+        evalscript = """
+            //VERSION=3
+
+            function setup() {
+                return {
+                    input: ["B02", "B03", "B04"],
+                    sampleType: "UINT16",
+                    output: { bands: 3 }
+                };
+            }
+
+            function evaluatePixel(sample) {
+                return [2.5 * sample.B04, 2.5 * sample.B03, 2.5 * sample.B02];
+            }
+        """
+
+        request = SentinelHubRequest(
+            evalscript=evalscript,
+            input_data=[
+                SentinelHubRequest.input_data(
+                    data_source=DataSource.SENTINEL2_L1C,
+                    time_interval=('2017-12-15T07:12:03', '2017-12-15T07:12:04'),
+                    maxcc=0.8
+                )
+            ],
+            responses=[
+                SentinelHubRequest.output_response('default', 'image/tiff')
+            ],
+
+            bounds=BBox(bbox=[9.250556, 41.36838, 9.25238, 41.369741], crs=CRS.WGS84),
+            resolution=(10.0, 10.0),
+            mime_type=MimeType.TIFF
+        )
+
+        img = request.get_data(max_threads=3)
+        # import ipdb; ipdb.set_trace()
+
+        # self.assertEqual(img.shape, (856, 512, 3))
+        # self.test_numpy_data(img, exp_min=0, exp_max=255, exp_mean=74.92)
+
     def test_multipart_tar(self):
         """ Test downloading multiple outputs, packed into a TAR file
         """
