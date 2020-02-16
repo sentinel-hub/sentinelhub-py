adb0d28e17fea2a952da804008496ebca875a60e
AleksMat <matej.aleksandrov@sinergise.com>
2020-02-16 13:50:23 +0100

    added unit tests for DownloadClient

--- tests/test_download.py
+++ tests/test_download.py
@@ -2,9 +2,10 @@
 Unit tests for download utilities
 """
 import unittest
+import copy
 import os
 
-from sentinelhub import DownloadRequest, MimeType
+from sentinelhub import DownloadRequest, MimeType, DownloadClient
 from sentinelhub.exceptions import SHRuntimeWarning
 from sentinelhub.testing_utils import TestSentinelHub
 
@@ -53,6 +54,51 @@ class TestDownloadRequest(unittest.TestCase):
 
         with self.assertWarns(SHRuntimeWarning):
             request.get_storage_paths()
+
+
+class TestDownloadClient(TestSentinelHub):
+
+    @classmethod
+    def setUp(cls):
+        super().setUpClass()
+
+        cls.request = DownloadRequest(
+            url='https://roda.sentinel-hub.com/sentinel-s2-l1c/tiles/1/C/CV/2017/1/14/0/tileInfo.json',
+            headers={'Content-Type': MimeType.JSON.get_string()},
+            data_type='json',
+            save_response=True,
+            data_folder=cls.OUTPUT_FOLDER,
+            filename=None,
+            return_data=True
+        )
+
+    def test_single_download(self):
+        client = DownloadClient(redownload=False)
+
+        result = client.download(self.request)
+
+        self.assertTrue(isinstance(result, dict))
+
+        request_path, response_path = self.request.get_storage_paths()
+        self.assertTrue(os.path.isfile(request_path))
+        self.assertTrue(os.path.isfile(response_path))
+
+    def test_multiple_downloads(self):
+        client = DownloadClient(redownload=True, raise_download_errors=False)
+
+        request2 = copy.deepcopy(self.request)
+        request2.save_response = False
+        request2.return_data = False
+
+        request3 = copy.deepcopy(self.request)
+        request3.url += 'invalid'
+
+        with self.assertWarns(SHRuntimeWarning):
+            results = client.download([self.request, request2, request3])
+
+        self.assertTrue(isinstance(results, list))
+        self.assertEqual(len(results), 3)
+        self.assertTrue(results[1] is None and results[2] is None)
 
 
 if __name__ == "__main__":
