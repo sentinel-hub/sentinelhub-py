7a242b4a67d6a99a2ba91076f563718a2e2f634b
iovsn <jovan.visnjic@sinergise.com>
2020-02-26 12:37:34 +0100

    BboxSelector accepting BBox objects

--- examples/utils.py
+++ examples/utils.py
@@ -5,14 +5,17 @@ from ipywidgets import widgets as w
 
 
 class BboxSelector:
-    def __init__(self, center=(0, 0), zoom=2):
+    def __init__(self, bbox):
+        center = (bbox.min_y + bbox.max_y) / 2, (bbox.min_x + bbox.max_x) / 2
+        self.map = Map(center=center, zoom=12, scroll_wheel_zoom=True)
+
         control = DrawControl()
 
         control.rectangle = {
             "shapeOptions": {
-                "fillColor": "#fca45d",
-                "color": "#fca45d",
-                "fillOpacity": 0.5
+                "fillColor": "#fabd14",
+                "color": "#fa6814",
+                "fillOpacity": 0.2
             }
         }
 
@@ -21,20 +24,44 @@ class BboxSelector:
         control.circle = {}
         control.circlemarker = {}
         control.polygon = {}
+        control.edit = False
+        control.remove = False
 
         control.on_draw(self._handle_draw)
 
-        self.map = Map(center=(53, 354), zoom=5, scroll_wheel_zoom=True)
         self.map.add_control(control)
-
-        self.rectangle = Rectangle(bounds=((52, 354), (53, 360)))
-        self.map.add_layer(self.rectangle)
 
         self.bbox = None
         self.size = None
+        self.rectangle = None
+        self.add_rectangle(bbox.min_x, bbox.min_y, bbox.max_x, bbox.max_y)
 
-        # self.out = w.Output(layout=w.Layout(width='100%', height='15px', overflow_y='scroll'))
+        # self.out = w.Output(layout=w.Layout(width='100%', height='50px', overflow_y='scroll'))
         # self.vbox = w.VBox([self.map, self.out])
+
+    def add_rectangle(self, min_x, min_y, max_x, max_y):
+        if self.rectangle:
+            self.map.remove_layer(self.rectangle)
+
+        self.rectangle = Rectangle(
+            bounds=((min_y, min_x), (max_y, max_x)),
+            color="#fa6814",
+            fill=True,
+            fill_color="#fabd14",
+            fill_opacity=0.2,
+            weight=1
+        )
+
+        self.map.add_layer(self.rectangle)
+
+        self.bbox = BBox(((min_x, min_y), (max_x, max_y)), CRS.WGS84).transform(CRS.POP_WEB)
+
+        # self.out.append_display_data((min_x, min_y, max_x, max_y))
+
+        size_x = abs(int((self.bbox.max_x - self.bbox.min_x) / 10))
+        size_y = abs(int((self.bbox.max_y - self.bbox.min_y) / 10))
+
+        self.size = size_x, size_y
 
     def _handle_draw(self, control, action, geo_json):
         control.clear_rectangles()
@@ -44,23 +71,7 @@ class BboxSelector:
         min_x, min_y = bbox_geom[0]
         max_x, max_y = bbox_geom[2]
 
-        bounds = ((min_y, min_x), (max_y, max_x))
-        # self.out.append_display_data(bounds)
-
-        self.map.remove_layer(self.rectangle)
-        self.rectangle = Rectangle(
-            bounds=bounds,
-            color="#1ca45d",
-            weight=1
-            )
-        self.map.add_layer(self.rectangle)
-
-        self.bbox = BBox(((min_x, min_y), (max_x, max_y)), CRS.WGS84).transform(CRS.POP_WEB)
-
-        size_x = abs(int((self.bbox.max_x - self.bbox.min_x) / 10))
-        size_y = abs(int((self.bbox.max_y - self.bbox.min_y) / 10))
-
-        self.size = size_x, size_y
+        self.add_rectangle(min_x, min_y, max_x, max_y)
 
     def show(self):
         return self.map
