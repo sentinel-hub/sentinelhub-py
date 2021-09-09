012cfd8f62b44c75b94c5edb29ef293af48ca6db
maleksandrov <matej.aleksandrov@sinergise.com>
2021-09-09 18:26:29 +0200

    removed a few deprecated functions and parameters

--- sentinelhub/download/__init__.py
+++ sentinelhub/download/__init__.py
@@ -3,7 +3,7 @@ A download part of the package
 """
 
 from .request import DownloadRequest
-from .client import DownloadClient, get_json, get_xml
+from .client import DownloadClient
 from .sentinelhub_client import SentinelHubDownloadClient
 from .sentinelhub_statistical_client import SentinelHubStatisticalDownloadClient
 from .aws_client import AwsDownloadClient
