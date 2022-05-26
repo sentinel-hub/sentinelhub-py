ca59ede28eedd4195b2ea99b5213e5be40a4106d
Ziga Luksic <ziga.luksic@sinergise.com>
2022-05-26 15:41:31 +0200

    try invoking through conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -253,3 +253,18 @@ def process_readme():
 
 
 process_readme()
+
+
+def run_apidoc(_):
+    from sphinx.ext.apidoc import main
+    import os
+    import sys
+
+    sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+    cur_dir = os.path.abspath(os.path.dirname(__file__))
+    module = os.path.join(cur_dir, "..", "sentinelhub")
+    main(["-e", "-o", cur_dir, module, os.path.join(module, "commands.py"), "--separate", "--no-toc"])
+
+
+def setup(app):
+    app.connect("builder-inited", run_apidoc)
