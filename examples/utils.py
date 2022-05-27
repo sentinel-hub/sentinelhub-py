84e67afc6fd6026d79760095d30143182de85def
maleksandrov <matej.aleksandrov@sinergise.com>
2022-05-27 11:31:36 +0200

    Added types into utils.py script

--- examples/utils.py
+++ examples/utils.py
@@ -1,14 +1,16 @@
 """
 Utilities used by example notebooks
 """
+from typing import Any, Optional, Tuple
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_image(image, factor=1.0, clip_range=None, **kwargs):
-    """
-    Utility function for plotting RGB images.
-    """
+def plot_image(
+    image: np.ndarray, factor: float = 1.0, clip_range: Optional[Tuple[float, float]] = None, **kwargs: Any
+) -> None:
+    """Utility function for plotting RGB images."""
     fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(15, 15))
     if clip_range is not None:
         ax.imshow(np.clip(image * factor, *clip_range), **kwargs)
