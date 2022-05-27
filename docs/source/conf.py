bc15913ff6b2cf8b84ad51f0bf0a3c515e1e263a
Ziga Luksic <ziga.luksic@sinergise.com>
2022-05-27 10:59:16 +0200

    apply isort and black

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,8 +13,8 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
-import sys
 import shutil
+import sys
 
 # -- Project information -----------------------------------------------------
 
