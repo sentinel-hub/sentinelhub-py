9d3f013a2d93eaf2fbadeea555ffb18b9c94cc0f
Ziga Luksic <ziga.luksic@sinergise.com>
2022-07-07 13:47:23 +0200

    remove rasterio skips from tests

--- tests/test_io_utils.py
+++ tests/test_io_utils.py
@@ -22,11 +22,6 @@ from sentinelhub.exceptions import SHUserWarning
     ],
 )
 def test_img_read(input_folder, output_folder, filename, mean, shape):
-    if filename == "img-15bit.jp2" and sys.version_info >= (3, 10):
-        pytest.skip(
-            "Rasterio doesn't support Python 3.10 yet, therefore JPEG2000 images would not be decoded correctly."
-        )
-
     is_jpeg = filename.endswith("jpg")
     img = read_data(os.path.join(input_folder, filename))
 
