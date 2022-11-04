ca87c14d25936a7e65cab35f5c2f775f1c33c435
jgersak <jan.gersak@sinergise.com>
2022-11-04 11:58:12 +0100

    update io_utils test add test for txt and xml make jpg test for himself

--- tests/test_io_utils.py
+++ tests/test_io_utils.py
@@ -1,9 +1,10 @@
 import os
-from platform import python_implementation
 from typing import Tuple
+from xml.etree import ElementTree as ET
 
 import numpy as np
 import pytest
+from fs.tempfs import TempFS
 from pytest import approx
 
 from sentinelhub import read_data, write_data
@@ -14,7 +15,6 @@ from sentinelhub.exceptions import SHUserWarning
     "filename, mean, shape",
     [
         ("img.tif", 13577.494856, (2048, 2048, 3)),
-        ("img.jpg", 52.412425, (2048, 2048, 3)),
         ("img.png", 52.33736, (2048, 2048, 3)),
         ("img-8bit.jp2", 47.09060, (343, 343, 3)),
         ("img-15bit.jp2", 0.3041897, (1830, 1830)),
@@ -22,27 +22,60 @@ from sentinelhub.exceptions import SHUserWarning
     ],
 )
 def test_img_read(input_folder: str, output_folder: str, filename: str, mean: float, shape: Tuple[int, ...]) -> None:
-    is_jpeg = filename.endswith("jpg")
+    img = read_data(os.path.join(input_folder, filename))
+
+    if filename.endswith("txt"):
+        img = np.fromstring(img)
+
+    assert img.shape == shape
+    assert np.mean(img) == approx(mean, abs=1e-4)
+    assert img.flags["WRITEABLE"], "Obtained numpy array is not writeable"
+
+    output_file_path = os.path.join(output_folder, filename)
+    write_data(output_file_path, img)
+    new_img = read_data(output_file_path)
+
+    assert np.array_equal(img, new_img), "Original and saved image are not the same"
+
+
+@pytest.mark.parametrize("filename, mean, shape", [("img.jpg", 52.412425, (2048, 2048, 3))])
+def test_img_read_jpeg(
+    input_folder: str, output_folder: str, filename: str, mean: float, shape: Tuple[int, ...]
+) -> None:
     img = read_data(os.path.join(input_folder, filename))
 
     assert img.shape == shape
-
-    if is_jpeg or python_implementation() != "PyPy":
-        assert np.mean(img) == approx(mean, abs=1e-4)
-
+    assert np.mean(img) == approx(mean, abs=1e-4)
     assert img.flags["WRITEABLE"], "Obtained numpy array is not writeable"
 
-    file_path = os.path.join(output_folder, filename)
+    output_file_path = os.path.join(output_folder, filename)
 
-    if is_jpeg:
-        with pytest.warns(SHUserWarning):
-            write_data(file_path, img)
-    else:
-        write_data(file_path, img)
-    new_img = read_data(file_path)
+    with pytest.warns(SHUserWarning):
+        write_data(output_file_path, img)
 
-    if not is_jpeg:
-        assert np.array_equal(img, new_img), "Original and saved image are not the same"
+
+def test_read_write_text() -> None:
+    with TempFS() as filesystem:
+        file_path = os.path.join(filesystem.root_path, "test-string.txt")
+
+        text = "sentinelhub-py is often shortened to sh-py"
+        write_data(file_path, text)
+        assert text == read_data(file_path)
+
+
+def test_read_write_xml() -> None:
+    with TempFS() as filesystem:
+        file_path = os.path.join(filesystem.root_path, "test-xml.xml")
+
+        root = ET.Element("EOPatch")
+        data = ET.SubElement(root, "data")
+        ET.SubElement(data, "field1", name="BANDS-S2-L1C").text = "some value1"
+        ET.SubElement(data, "field2", name="CLP").text = "some value2"
+        tree = ET.ElementTree(root)
+
+        write_data(file_path, tree)
+        tree_new = read_data(file_path)
+        assert set(tree_new.getroot().itertext()) == set(tree.getroot().itertext())
 
 
 def test_read_tar_with_folder(input_folder: str) -> None:
