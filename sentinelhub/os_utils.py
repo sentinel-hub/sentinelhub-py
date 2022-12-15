00b5a16119f08939143d43030933476a2e69d9d4
Ziga Luksic <ziga.luksic@sinergise.com>
2022-12-15 09:43:06 +0100

    improve warning message

--- sentinelhub/os_utils.py
+++ sentinelhub/os_utils.py
@@ -14,7 +14,8 @@ from .exceptions import SHDeprecationWarning
 def deprecated(func: Callable) -> Callable:  # pylint: disable=missing-docstring
     def deprecated_func(*args, **kwargs):  # type: ignore
         warnings.warn(
-            "The `sentinelhub.os_utils` module is deprecated and will be removed in future version.",
+            f"Function `{func.__name__}` is part of `sentinelhub.os_utils` module, which is deprecated and will be"
+            " removed in future version.",
             category=SHDeprecationWarning,
             stacklevel=2,
         )
