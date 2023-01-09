8952585cdb885c5795bb2ffd640a55690f522d41
Žiga Lukšič <31988337+zigaLuksic@users.noreply.github.com>
2023-01-09 09:50:54 +0100

    Update sentinelhub/type_utils.py
    Co-authored-by: jgersak <112631680+jgersak@users.noreply.github.com>

--- sentinelhub/type_utils.py
+++ sentinelhub/type_utils.py
@@ -5,7 +5,7 @@ from .exceptions import SHDeprecationWarning
 from .types import *  # noqa # pylint: disable=wildcard-import,unused-wildcard-import
 
 warn(
-    "The module `sentinelhub.types` is deprecated, use `sentinelhub.types` instead.",
+    "The module `sentinelhub.type_utils` is deprecated, use `sentinelhub.types` instead.",
     category=SHDeprecationWarning,
     stacklevel=2,
 )
