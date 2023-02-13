3aafac7165120f256e1e80fef5285383d9ef4018
jgersak <jan.gersak@sinergise.com>
2023-02-13 11:58:47 +0100

    Update tests
    split test cases that test fultiple functions or faild cases

--- tests/test_data_collections.py
+++ tests/test_data_collections.py
@@ -9,14 +9,6 @@ from sentinelhub import DataCollection
 from sentinelhub.data_collections import DataCollectionDefinition
 
 
-def test_repr() -> None:
-    definition = DataCollection.SENTINEL1_IW.value
-    representation = repr(definition)
-
-    assert isinstance(representation, str)
-    assert representation.count("\n") >= 5
-
-
 def test_derive() -> None:
     definition = DataCollectionDefinition(api_id="X", wfs_id="Y")
     derived_definition = definition.derive(wfs_id="Z")
@@ -24,6 +16,11 @@ def test_derive() -> None:
     assert derived_definition.api_id == "X"
     assert derived_definition.wfs_id == "Z"
     assert derived_definition.collection_type is None
+
+
+def test_collection_string() -> None:
+    dcd = DataCollectionDefinition(api_id="X", _name="A")
+    assert str(dcd) == "DataCollectionDefinition(\n  api_id: X\n  is_timeless: False\n  has_cloud_coverage: False\n)"
 
 
 def test_compare() -> None:
@@ -34,10 +31,7 @@ def test_compare() -> None:
 
 
 def test_define() -> None:
-    for _ in range(3):
-        data_collection = DataCollection.define(
-            "NEW", api_id="X", sensor_type="Sensor", bands=("B01",), is_timeless=True
-        )
+    data_collection = DataCollection.define("NEW", api_id="X", sensor_type="Sensor", bands=("B01",), is_timeless=True)
 
     assert data_collection == DataCollection.NEW
 
@@ -50,8 +44,7 @@ def test_define() -> None:
 
 def test_define_from() -> None:
     bands = ["B01", "XYZ"]
-    for _ in range(3):
-        data_collection = DataCollection.define_from(DataCollection.SENTINEL5P, "NEW_5P", api_id="X", bands=bands)
+    data_collection = DataCollection.define_from(DataCollection.SENTINEL5P, "NEW_5P", api_id="X", bands=bands)
 
     assert data_collection == DataCollection.NEW_5P
     assert data_collection.api_id == "X"
@@ -59,17 +52,22 @@ def test_define_from() -> None:
     assert data_collection.bands == tuple(bands)
 
 
-def test_define_byoc_and_batch() -> None:
+def test_define_byoc() -> None:
     byoc_id = "0000d273-7e89-4f00-971e-9024f89a0000"
     byoc = DataCollection.define_byoc(byoc_id, name="MY_BYOC")
-    batch = DataCollection.define_batch(byoc_id, name="MY_BATCH")
 
     assert byoc == DataCollection.MY_BYOC
-    assert batch == DataCollection.MY_BATCH
+    assert byoc.api_id.endswith(byoc_id)
+    assert byoc.collection_id == byoc_id
 
-    for data_collection in [byoc, batch]:
-        assert data_collection.api_id.endswith(byoc_id)
-        assert data_collection.collection_id == byoc_id
+
+def test_define_batch() -> None:
+    batch_id = "0000d273-7e89-4f00-971e-9024f89a0000"
+    batch = DataCollection.define_batch(batch_id, name="MY_BATCH")
+
+    assert batch == DataCollection.MY_BATCH
+    assert batch.api_id.endswith(batch_id)
+    assert batch.collection_id == batch_id
 
 
 def test_attributes() -> None:
@@ -80,6 +78,8 @@ def test_attributes() -> None:
         assert value is not None
         assert value == getattr(data_collection.value, attr_name)
 
+
+def test_attributes_empty_fail() -> None:
     data_collection = DataCollection.define("EMPTY")
 
     for attr_name in ["api_id", "catalog_id", "wfs_id", "bands"]:
@@ -89,25 +89,41 @@ def test_attributes() -> None:
     assert data_collection.service_url is None
 
 
-def test_sentinel1_checks() -> None:
-    assert DataCollection.SENTINEL1_IW.is_sentinel1
-    assert not DataCollection.SENTINEL2_L1C.is_sentinel1
+@pytest.mark.parametrize(
+    "collection, collection_type, expaected",
+    [
+        ("SENTINEL2_L1C", "is_sentinel1", False),
+        ("SENTINEL2_L1C", "is_byoc", False),
+        ("SENTINEL2_L1C", "is_batch", False),
+        ("SENTINEL1_EW", "is_sentinel1", True),
+        ("SENTINEL1_EW", "is_byoc", False),
+        ("SENTINEL1_EW", "is_batch", False),
+    ],
+)
+def test_sentinel_is_checks(collection: str, collection_type: str, expaected: bool) -> None:
+    data_collection = getattr(DataCollection, collection)
+    assert getattr(data_collection, collection_type) == expaected
 
-    assert DataCollection.SENTINEL1_IW_ASC.contains_orbit_direction("ascending")
-    assert not DataCollection.SENTINEL1_IW_DES.contains_orbit_direction("ascending")
 
-    assert DataCollection.SENTINEL2_L2A.contains_orbit_direction("descending")
+@pytest.mark.parametrize(
+    "collection, direction, expaected",
+    [
+        ("SENTINEL1_IW_ASC", "ascending", True),
+        ("SENTINEL1_IW_ASC", "descending", False),
+        ("SENTINEL1_IW_DES", "ascending", False),
+        ("SENTINEL2_L2A", "descending", True),
+        ("SENTINEL2_L2A", "ascending", True),
+    ],
+)
+def test_contains_orbit_direction(collection: str, direction: str, expaected: bool) -> None:
+    data_collection = getattr(DataCollection, collection)
+    assert data_collection.contains_orbit_direction(direction) == expaected
 
 
 def test_get_available_collections() -> None:
     collections = DataCollection.get_available_collections()
-    assert helper_check_collection_list(collections)
-
-
-def helper_check_collection_list(collection_list: Any) -> bool:
-    is_list = isinstance(collection_list, list)
-    contains_collections = all(isinstance(data_collection, DataCollection) for data_collection in collection_list)
-    return is_list and contains_collections
+    assert len(collections) >= 28
+    assert all([type(collection) == DataCollection for collection in collections])
 
 
 def test_transfer_with_ray(ray: Any) -> None:
