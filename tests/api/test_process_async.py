38a71a9137dc17dff0cd8acc9ce1b2b5e87b0adb
Matic Lubej <matic.lubej@sinergise.com>
2023-03-27 15:10:34 +0200

    add test for async process api

---
+++ tests/api/test_process_async.py
@@ -0,0 +1,41 @@
+"""
+A module that tests an interface for Sentinel Hub Batch processing
+"""
+import datetime as dt
+
+import pytest
+from requests_mock import Mocker
+
+from sentinelhub import CRS, BBox, DataCollection, MimeType, SentinelHubRequest
+from sentinelhub.api.process import AsyncProcessRequest
+
+pytestmark = pytest.mark.sh_integration
+
+
+def test_create_and_run_batch_request(requests_mock: Mocker) -> None:
+    """A test that mocks creation and execution of a new batch request"""
+    evalscript = "some evalscript"
+    time_interval = dt.date(year=2020, month=6, day=1), dt.date(year=2020, month=6, day=10)
+    bbox = BBox((14.0, 45.8, 14.2, 46.0), crs=CRS.WGS84)
+    request = AsyncProcessRequest(
+        evalscript=evalscript,
+        input_data=[
+            SentinelHubRequest.input_data(
+                data_collection=DataCollection.SENTINEL2_L1C,
+                time_interval=time_interval,
+            )
+        ],
+        responses=[
+            SentinelHubRequest.output_response("B02", MimeType.TIFF),
+        ],
+        delivery=AsyncProcessRequest.s3_specification(url="s3_my_bucket", access_key="foo", secret_access_key="bar"),
+        bbox=bbox,
+    )
+
+    requests_mock.post("/oauth/token", real_http=True)
+    requests_mock.post(
+        "/api/v1/async/process",
+        [{"json": {"id": "beep", "status": "RUNNING"}}],
+    )
+
+    assert request.get_data() == [{"id": "beep", "status": "RUNNING"}]
