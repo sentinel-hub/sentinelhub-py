28002f9056888b64af8acffb46a23bf5b42740ef
Matic Lubej <matic.lubej@sinergise.com>
2023-03-30 17:02:19 +0200

    trim the fat in tests

--- tests/test_evalscript.py
+++ tests/test_evalscript.py
@@ -1,105 +1,100 @@
-import json
-import re
-from typing import List, Optional, Tuple
+from typing import List
 
 import pytest
 
 from sentinelhub import DataCollection
-from sentinelhub.data_collections_bands import Unit
 from sentinelhub.evalscript import generate_evalscript
 
 
-def parse_evalscript_info(evalscript: str) -> Tuple[List[str], List[str], List[Tuple[str, str, str]]]:
-    input_names_re = re.search(r"bands: (\[.*\])", evalscript)
-    input_units_re = re.search(r"units: (\[.*\])", evalscript)
-    output_spec = re.findall(r'\{ id: "([^\s.]+)", bands: ([^\s.]+), sampleType: "([^\s.]+)" \}', evalscript)
-
-    assert input_names_re is not None, "regex failed for extraction of input names"
-    assert input_units_re is not None, "regex failed for extraction of input units"
-
-    input_names = json.loads(input_names_re.group(1))
-    input_units = json.loads(input_units_re.group(1))
-
-    return input_names, input_units, output_spec
+@pytest.mark.parametrize("data_collection", [DataCollection.SENTINEL2_L1C, DataCollection.SENTINEL1_IW])
+def test_collection_bands(data_collection: DataCollection) -> None:
+    evalscript = generate_evalscript(data_collection=data_collection)
+    expected_bands_str = "bands: [" + ", ".join(f'"{b.name}"' for b in data_collection.bands) + "]"
+    assert expected_bands_str in evalscript
 
 
 @pytest.mark.parametrize(
     "data_collection, bands",
     [
-        (DataCollection.SENTINEL2_L1C, None),
-        (DataCollection.SENTINEL2_L1C, ["B04", "B03", "B02"]),
+        (DataCollection.SENTINEL2_L2A, ["B04", "B03", "B02"]),
         (DataCollection.SENTINEL1_IW, ["VV", "VH"]),
     ],
+    ids=str,
 )
-def test_input_bands(data_collection: DataCollection, bands: Optional[List[str]]) -> None:
+def test_explicit_bands(data_collection: DataCollection, bands: List[str]) -> None:
     evalscript = generate_evalscript(data_collection=data_collection, bands=bands)
-    input_names, *_ = parse_evalscript_info(evalscript)
-    assert len(input_names) == len(bands) if bands is not None else len(data_collection.bands)
+    expected_bands_str = "bands: [" + ", ".join(f'"{b}"' for b in bands) + "]"
+    assert expected_bands_str in evalscript
 
 
 @pytest.mark.parametrize(
     "meta_bands",
-    [None, ["CLM"], ["CLP", "dataMask"]],
+    [["CLM"], ["CLP", "dataMask"]],
+    ids=str,
 )
-def test_input_meta_bands(meta_bands: Optional[List[str]]) -> None:
+def test_explicit_meta_bands(meta_bands: List[str]) -> None:
     data_collection = DataCollection.SENTINEL2_L1C
     evalscript = generate_evalscript(data_collection=data_collection, meta_bands=meta_bands)
-    input_names, *_ = parse_evalscript_info(evalscript)
 
-    num_meta_bands = 0 if meta_bands is None else len(meta_bands)
-    assert len(input_names) == len(data_collection.bands) + num_meta_bands
-
-
-@pytest.mark.parametrize("use_dn", [True, False])
-@pytest.mark.parametrize(
-    "data_collection, bands, meta_bands",
-    [
-        (DataCollection.SENTINEL2_L1C, ["B04", "B03", "B02"], ["CLP", "sunZenithAngles"]),
-        (DataCollection.SENTINEL3_OLCI, ["B04", "B03"], ["HUMIDITY"]),
-    ],
-)
-def test_input_units(data_collection: DataCollection, bands: List[str], meta_bands: List[str], use_dn: bool) -> None:
-    evalscript = generate_evalscript(data_collection=data_collection, bands=bands, meta_bands=meta_bands, use_dn=use_dn)
-    input_names, input_units, _ = parse_evalscript_info(evalscript)
-
-    band_info_dict = {band_info.name: band_info for band_info in data_collection.bands + data_collection.metabands}
-    for name, unit in zip(input_names, input_units):
-        band = band_info_dict[name]
-        assert Unit(unit) in band.units
-
-        if use_dn and Unit.DN in band.units:
-            assert Unit(unit) == Unit.DN
+    all_bands = [f"{b.name}" for b in data_collection.bands] + meta_bands
+    expected_bands_str = "bands: [" + ", ".join(f'"{b}"' for b in all_bands) + "]"
+    assert expected_bands_str in evalscript
 
 
 @pytest.mark.parametrize(
     "data_collection, bands, meta_bands",
     [
-        (DataCollection.LANDSAT_TM_L2, None, ["BQA"]),
+        (DataCollection.LANDSAT_TM_L2, ["B05", "B04", "B03", "B02"], ["BQA"]),
         (DataCollection.SENTINEL2_L2A, ["B04", "B03", "B02"], ["CLP", "SCL"]),
     ],
+    ids=str,
 )
-def test_merged_output(data_collection: DataCollection, bands: Optional[List[str]], meta_bands: List[str]) -> None:
+def test_merged_output(data_collection: DataCollection, bands: List[str], meta_bands: List[str]) -> None:
     merged_output = "merged_bands"
     evalscript = generate_evalscript(
         data_collection=data_collection, bands=bands, meta_bands=meta_bands, merged_output=merged_output
     )
-    *_, output_spec = parse_evalscript_info(evalscript)
 
-    num_bands = len(data_collection.bands) if bands is None else len(bands)
-    depth = [depth for name, depth, _ in output_spec if name == merged_output][0]
+    expected_bands_output_spec = "{ " + f'id: "{merged_output}", bands: {len(bands)}'
+    assert expected_bands_output_spec in evalscript
 
-    assert len(output_spec) == 1 + len(meta_bands)
-    assert int(depth) == num_bands
+    expected_bands_return_spec = f"{merged_output}: [" + ", ".join(f"sample.{b}" for b in bands) + "]"
+    assert expected_bands_return_spec in evalscript
 
 
-@pytest.mark.parametrize("merged_output", [None, "merged_bands"])
-@pytest.mark.parametrize("use_dn", [True, False])
-def test_sample_type(merged_output: Optional[str], use_dn: bool) -> None:
+def test_units() -> None:
+    bands = ["B04", "B03", "B02", "B02"]
+    data_collection = DataCollection.SENTINEL2_L1C
+
+    evalscript_dn = generate_evalscript(data_collection=data_collection, bands=bands)
+    assert evalscript_dn.count('"DN"') == len(bands)
+    assert evalscript_dn.count('"REFLECTANCE"') == 0
+
+    evalscript_reflectance = generate_evalscript(data_collection=data_collection, bands=bands, use_dn=False)
+    assert evalscript_reflectance.count('"REFLECTANCE"') == len(bands)
+    assert evalscript_reflectance.count('"DN"') == 0
+
+
+@pytest.mark.parametrize("use_dn", [True, False], ids=lambda x: f"use_dn: {x}")
+def test_sample_type(use_dn: bool) -> None:
+    data_collection = DataCollection.SENTINEL2_L1C
+    evalscript = generate_evalscript(data_collection=data_collection, use_dn=use_dn)
+
+    expected_uint_count = len(data_collection.bands) if use_dn else 0
+    assert evalscript.count('"UINT16"') == expected_uint_count
+
+    expected_float_count = len(data_collection.bands) if not use_dn else 0
+    assert evalscript.count('"FLOAT32"') == expected_float_count
+
+
+@pytest.mark.parametrize("use_dn", [True, False], ids=lambda x: f"use_dn: {x}")
+def test_sample_type_merged(use_dn: bool) -> None:
+    merged_output = "merged_bands"
     data_collection = DataCollection.SENTINEL2_L1C
     evalscript = generate_evalscript(data_collection=data_collection, merged_output=merged_output, use_dn=use_dn)
 
-    *_, output_spec = parse_evalscript_info(evalscript)
-    assert len(output_spec) == 1 if merged_output else len(data_collection.bands)
+    expected_uint_count = 1 if use_dn else 0
+    assert evalscript.count('"UINT16"') == expected_uint_count
 
-    sample_types_set = {sample_type for *_, sample_type in output_spec}
-    assert sample_types_set == {"UINT16" if use_dn else "FLOAT32"}
+    expected_float_count = 1 if not use_dn else 0
+    assert evalscript.count('"FLOAT32"') == expected_float_count
